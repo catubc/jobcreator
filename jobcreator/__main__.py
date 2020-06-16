5534522d1b2ae25cda2ed53d68559ffeb8a3a508
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2020-06-16 22:35:49 +0200

    first prototype

---
+++ jobcreator/__main__.py
@@ -0,0 +1,46 @@
+import argparse
+import json
+import os
+import subprocess
+
+from ._job_templates import suite2p_job_file, caiman_job_file
+
+JOB_FILE_GENERATORS = {
+    'suite2p': suite2p_job_file,
+    'caiman': caiman_job_file
+}
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Pipeline parameters')
+    parser.add_argument('--pipeline', default=[], type=str, help='options')
+    parser.add_argument('--settings', default=[], type=str, help='options')
+    parser.add_argument('--output', default='.', type=str, help='options')
+
+    args = parser.parse_args()
+
+    pipeline_name = args.pipeline
+    settings_path = args.settings
+    output_path = args.output
+
+    return pipeline_name, settings_path, output_path
+
+
+def main():
+    pipeline_name, settings_path, output_path = parse_args()
+
+    job_file_path = os.path.join(output_path, 'job_file.sh')
+
+    with open(settings_path, "r") as read_file:
+        job_settings = json.load(read_file)
+
+    # get the file text
+    job_file_gen = JOB_FILE_GENERATORS[pipeline_name]
+    job_file_text = job_file_gen(**job_settings)
+
+    # make the file job file
+    with open(job_file_path, "w") as text_file:
+        text_file.write(job_file_text)
+
+    # run the job
+    subprocess.run(['srun', job_file_path])
---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+
+from setuptools import find_packages, setup
+
+install_requires = [
+    line.rstrip() for line in open(
+        os.path.join(os.path.dirname(__file__), "requirements.txt")
+    )
+]
+
+setup(name='jobcreator',
+      install_requires=install_requires,
+      version='0.0.1',
+      description='Imaging database and storage',
+      url='https://github.com/donatolab/jobcreator',
+      license='MIT',
+      packages=find_packages(),
+      entry_points={
+          'console_scripts': ['jobcreator=jobcreator.__main__:main'],
+      },
+      zip_safe=False)
