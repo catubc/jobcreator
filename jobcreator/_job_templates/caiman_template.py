6f0c38a120b40bb796066379a5c8526f472d494b
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2020-08-31 11:29:38 +0200

    add job_id

--- jobcreator/_job_templates/caiman_template.py
+++ jobcreator/_job_templates/caiman_template.py
@@ -136,7 +136,7 @@ echo "loading env"
 source /tungstenfs/scratch/garber/keviny/caiman_test/.venv/bin/activate
 
 echo "saving environment information"
-pip freeze > {env_file_stub}%j.txt
+pip freeze > "{env_file_stub} $SLURM_JOB_ID .txt"
 
 # move the files
 for file in {data_pattern}; do cp "$file" {jobcreator_output_dir};done
