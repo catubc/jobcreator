8514c52776275cebecc0a54a1e3ed874c2daaa5b
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2020-11-16 22:31:03 +0100

    add caiman desktop runner

--- jobcreator/__main__.py
+++ jobcreator/__main__.py
@@ -9,6 +9,7 @@ from ._job_templates import (
     caiman_job_file_fmi,
     caiman_mcorr_job_fmi,
 )
+from ._pipeline_runners.caiman.caiman_desktop_runner import caiman_desktop_runner
 from ._pipeline_checks import check_caiman
 
 JOB_FILE_GENERATORS = {
@@ -18,6 +19,7 @@ JOB_FILE_GENERATORS = {
     "caiman-mcorr-fmi": caiman_mcorr_job_fmi,
 }
 PIPELINE_CHECKERS = {"caiman": check_caiman}
+DESKTOP_RUNNERS = {"caiman": caiman_desktop_runner}
 
 
 def parse_args():
@@ -68,3 +70,27 @@ def main():
 
         # run the job
         subprocess.run(["sbatch", job_file_path])
+
+
+def desktop_runner():
+    pipeline_name, settings_path, output_dir, test = parse_args()
+
+    if test is True:
+        checker_function = PIPELINE_CHECKERS[pipeline_name]
+        checker_function()
+    else:
+        # make the output directory if it doesn't exist
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        else:
+            raise ValueError("output must be saved to a new directory")
+
+        with open(settings_path, "r") as read_file:
+            job_settings = json.load(read_file)
+
+        # add the output directory to the job file settings
+        job_settings["jobcreator_output_dir"] = output_dir
+
+        # get the file text
+        runner = DESKTOP_RUNNERS[pipeline_name]
+        runner(**job_settings)
